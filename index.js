83de27e268c674fc0b3a21c385472fb10ad5a289
Aleksandr Pavliuk <pavliuk.aleksandr@gmail.com>
2020-11-22 08:33:59 +0200

    Project start commit

---
+++ index.js
@@ -0,0 +1,20 @@
+// Required
+const http = require('http')
+const UsersRepository = require('./modules/UsersRepository')
+// Constants
+const usersRepository = new UsersRepository()
+
+const hostname = '127.0.0.1'
+const port = 3000
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200
+  res.setHeader('Content-Type', 'text/plain')
+
+  var count = usersRepository.increase()
+  res.end(`Hello from the server! You are ${count}th visitor.`)
+})
+
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`)
+})
---
+++ modules/UsersRepository.js
@@ -0,0 +1,29 @@
+const fs = require('fs')
+const paths = {
+  STORAGE: './storage/users.json',
+}
+
+module.exports = class UsersRepository {
+  constructor() {}
+
+  // MARK : Private
+  static getData() {
+    const data = fs.readFileSync(paths.STORAGE, 'utf-8')
+    return JSON.parse(data)
+  }
+
+  // MARK : Public
+  theCount() {
+    const dataObj = UsersRepository.getData()
+    return dataObj[0].count
+  }
+
+  increase() {
+    const dataObj = UsersRepository.getData()
+    dataObj[0].count = dataObj[0].count + 1
+    var resultAtString = JSON.stringify(dataObj)
+    fs.writeFileSync(paths.STORAGE, resultAtString)
+
+    return dataObj[0].count
+  }
+}
